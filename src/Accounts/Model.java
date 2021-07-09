67d8ec9ce264aca144b486e72d11916555996c32
chamRun <chrm@aut.ac.ir>
2021-07-09 15:21:12 +0430

    Login's Done
    Now I should work on signing up.

--- src/Accounts/Model.java
+++ src/Accounts/Model.java
@@ -1,15 +1,18 @@
 package Accounts;
 
-import org.apache.commons.io.FileUtils;
-
-import java.io.*;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.*;
-import java.util.Collection;
 
 public class Model {
+
+    public static boolean signUp(String email, String password){
+
+
+        return false;
+
+    }
 
 
     public static User tryLogIn(String email, String password){
@@ -22,7 +25,7 @@ public class Model {
 
             Statement statement = connection.createStatement();
 
-            String cmd = "SELECT pass FROM test.dbo.users\n" +
+            String cmd = "SELECT * FROM test.dbo.users\n" +
                     "WHERE uname = 'ali'";
 
             ResultSet resultSet = statement.executeQuery(cmd);
@@ -30,7 +33,7 @@ public class Model {
             // Print results from select statement
             while (resultSet.next()) {
 
-                if (resultSet.getString("pass").equals(password)){
+                if (resultSet.getString("pass").startsWith(password)){
                     return new User(email, password);
                 }
 
@@ -69,64 +72,6 @@ public class Model {
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
-    }
-
-
-        public static boolean signUp(String email, String password){
-
-        File directory = new File("Accounts/UserFiles");
-
-        Collection<File> files = FileUtils.listFiles(directory,
-                new String[] {"txt"}, true);
-
-
-        try {
-            for (File f : files) {
-
-                FileInputStream fis = new FileInputStream(f);
-                ObjectInputStream ois = new ObjectInputStream(fis);
-
-                try {
-                    User temp;
-                    temp = (User) ois.readObject();
-                    if (email.equals(temp.getEmail())){
-                        return false;
-                    }
-                    fis.close();
-                    ois.close();
-
-                } catch (EOFException ignored){
-                } catch (ClassNotFoundException | IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-        File file = new File("UserFiles\\" + email + ".txt");
-
-        try {
-            FileUtils.touch(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (FileOutputStream fos = new FileOutputStream("UserFiles\\" + email + ".txt")){
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-
-            oos.writeObject(new User(email, password));
-            fos.close();
-            oos.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(email + " was signed up");
-        return true;
-
     }
 
 
